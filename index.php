17e99aab54eeaa8dc71a5edb5d13c50fc0d8b801
Chirag Jain <jain0318@gmail.com>
2018-05-19 13:37:48 +0530

    Add example code

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Hello 55gg";
+switch (n) {
+    case label1:
+        code to be executed if n=label1;
+        break;
+    case label2:
+        code to be executed if n=label2;
+        break;
+    case label3:
+        code to be executed if n=label3;
+        break;
+   
+    default:
+        code to be executed if n is different from all labels;
+} 
