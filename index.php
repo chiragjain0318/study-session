5b460bb9d15d2f83a3e0a142407b0fbf183ae492
Chirag Jain <jain0318@gmail.com>
2018-05-19 13:42:51 +0530

    Chnage label

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ switch (n) {
     case label2:
         code to be executed if n=label2;
         break;
-    case label3:
-        code to be executed if n=label3;
+    case label4:
+        code to be executed if n=label4;
         break;
    
     default:
